7e6605b261565e8a4c4c6cd7f956bff1bc42ff01
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-02-25 21:58:02 -0400

    Codigo com comentarios

--- SQL-Alchemy.py/consulta_or.py
+++ SQL-Alchemy.py/consulta_or.py
@@ -1,25 +1,34 @@
-from sqlalchemy import create_engine, or_
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from ORM import Pessoa
 
+# Definindo as informações de autenticação e configuração do banco de dados
+USUARIO = "maia"
+SENHA ="12149180"
+HOST = "localhost"
+BANCO = "aulaPythonFull"
+PORT = "3306"
 
-def RetornaSession():
-    USUARIO = "maia"
-    SENHA ="12149180"
-    HOST = "localhost"
-    BANCO = "aulaPythonFull"
-    PORT = "3306"
+# Criando a string de conexão com o banco de dados
+CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
 
-    CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
+# Criando a conexão com o banco de dados MySQL
+engine = create_engine(CONN, echo=True)
 
-    engine = create_engine(CONN, echo=True)
-    Session = sessionmaker(bind=engine)
+# Criando uma instância da classe sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    return Session()
+# Criando a classe base ORM
+Base = declarative_base()
 
-session = RetornaSession()
+# Criando a classe Pessoa ORM que representa a tabela "Pessoa" do banco de dados
+class Pessoa(Base):
+    __tablename__ = "Pessoa"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    usuario = Column(String(20))
+    senha = Column(String(10))
 
-x = session.query(Pessoa).filter(or_(Pessoa.nome == 'Caio', Pessoa.nome == 'Marcos')).all()
-
-for i in x:
-    print(i.nome)
+# Criando todas as tabelas definidas nas classes ORM no banco de dados
+Base.metadata.create_all(engine)
